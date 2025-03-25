f236d9fe9ae83cc0c7cb8a508c24fcb908c6d0ee
Kobutaemon <isotoa1219@gmail.com>
2025-03-25 14:19:58 +0900

    Achievementsのリファクタリングをした

--- src/components/AchievementCard/AchievementCard.tsx
+++ src/components/AchievementCard/AchievementCard.tsx
@@ -1,119 +1,17 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
 import './AchievementCard.css'
-import { Pagination } from 'swiper/modules';
 
 function AchievementCard(props: {title: string, description: string }) { //写真のデータが来たらpropsに「img_url: string」を追加して差し替える。
   return (
     <div className='swiper-container'>
-      <Swiper
-        modules={[Pagination]}
-        spaceBetween={0}
-        slidesPerView={3}
-        pagination={true}
-      >
-      <SwiperSlide>
-        <div className="achievement-card">
-          <div className="dummy-img"></div>
-          <h3 className="achievement-card-title">
-            {props.title}
-          </h3>
-          <p className="achievement-card-description">
-            {props.description}
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="achievement-card">
-          <div className="dummy-img"></div>
-          <h3 className="achievement-card-title">
-            {props.title}
-          </h3>
-          <p className="achievement-card-description">
-            {props.description}
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="achievement-card">
-          <div className="dummy-img"></div>
-          <h3 className="achievement-card-title">
-            {props.title}
-          </h3>
-          <p className="achievement-card-description">
-            {props.description}
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="achievement-card">
-          <div className="dummy-img"></div>
-          <h3 className="achievement-card-title">
-            {props.title}
-          </h3>
-          <p className="achievement-card-description">
-            {props.description}
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="achievement-card">
-          <div className="dummy-img"></div>
-          <h3 className="achievement-card-title">
-            {props.title}
-          </h3>
-          <p className="achievement-card-description">
-            {props.description}
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="achievement-card">
-          <div className="dummy-img"></div>
-          <h3 className="achievement-card-title">
-            {props.title}
-          </h3>
-          <p className="achievement-card-description">
-            {props.description}
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="achievement-card">
-          <div className="dummy-img"></div>
-          <h3 className="achievement-card-title">
-            {props.title}
-          </h3>
-          <p className="achievement-card-description">
-            {props.description}
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="achievement-card">
-          <div className="dummy-img"></div>
-          <h3 className="achievement-card-title">
-            {props.title}
-          </h3>
-          <p className="achievement-card-description">
-            {props.description}
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="achievement-card">
-          <div className="dummy-img"></div>
-          <h3 className="achievement-card-title">
-            {props.title}
-          </h3>
-          <p className="achievement-card-description">
-            {props.description}
-          </p>
-        </div>
-      </SwiperSlide>
-    </Swiper>
-  
+      <div className="achievement-card">
+        <div className="dummy-img"></div>
+        <h3 className="achievement-card-title">
+          {props.title}
+        </h3>
+        <p className="achievement-card-description">
+          {props.description}
+        </p>
+      </div>
     </div>
   )
 }
--- src/components/Achievements/Achievements.tsx
+++ src/components/Achievements/Achievements.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import './Achievements.css'
+import { Pagination } from 'swiper/modules';
 import SectionTitle from '../SectionTitle/SectionTitle'
 import AchievementCard from '../AchievementCard/AchievementCard'
 
@@ -10,7 +13,31 @@ function Achievements(){
         <SectionTitle title='Achievements' />
       </div>
       <div className="achievements-content">
-        <AchievementCard title='Test Achievement' description='This is the test achievementThis is the test achievementThis is the test achievementThis is the test achievementThis is the test achievement'/>
+        <Swiper
+          modules={[Pagination]}
+          spaceBetween={0}
+          slidesPerView={3}
+          pagination={true}
+        >
+          <SwiperSlide>
+            <AchievementCard title='Test Achievement 1' description='This is the test achievementThis is the test achievementThis is the test achievementThis is the test achievementThis is the test achievement'/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <AchievementCard title='Test Achievement 2' description='This is the test achievementThis is the test achievementThis is the test achievementThis is the test achievementThis is the test achievement'/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <AchievementCard title='Test Achievement 3' description='This is the test achievementThis is the test achievementThis is the test achievementThis is the test achievementThis is the test achievement'/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <AchievementCard title='Test Achievement 4' description='This is the test achievementThis is the test achievementThis is the test achievementThis is the test achievementThis is the test achievement'/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <AchievementCard title='Test Achievement 5' description='This is the test achievementThis is the test achievementThis is the test achievementThis is the test achievementThis is the test achievement'/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <AchievementCard title='Test Achievement 6' description='This is the test achievementThis is the test achievementThis is the test achievementThis is the test achievementThis is the test achievement'/>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   )
