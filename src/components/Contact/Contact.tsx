663d198120a5f8848d9ae4522b9792d3737ad531
Kobutaemon <ceti1241010@gn.iwasaki.ac.jp>
2025-03-31 01:54:34 +0900

    Contactの背景以外完成

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import './Contact.css'
 import SectionTitle from '../SectionTitle/SectionTitle'
+import JoinButton from '../JoinButton/JoinButton'
 
 function Contact() {
   return (
-    <div>
+    <div className="contact-container">
       <div className="contact-title">
         <SectionTitle title="Contact" />
       </div>
-      
+      <div className="contact-sen">
+        <p>Make ITはMake IT は初心者・やる気のある人大歓迎！</p>
+        <p>お仕事のご依頼もお待ちしています。</p>
+        <p>仮入部・仕事のご依頼は下記のリンクから！</p>
+      </div>
+      <div className="contact-button">
+        <JoinButton />
+      </div>
     </div>
   )
 }
