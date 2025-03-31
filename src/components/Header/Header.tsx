5b54aff40369dd45e321651c9a5a566ba28fb6aa
Kobutaemon <isotoa1219@gmail.com>
2025-03-31 18:44:42 +0900

    ハンバーガーメニュー完成

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,26 @@
 import JoinButton from '../JoinButton/JoinButton'
 import './Header.css'
 
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburger');
+
+if (hamburger && menu) {
+  hamburger.addEventListener('click', function(){
+    menu.classList.toggle("open");
+    hamburger.classList.toggle('active');
+  });
+}
+
 function Header() {
   return (
     <header>
       <img src="./assets/makeit_logo.png" alt="MakeITのロゴ" />
-      <nav>
+      <nav className='menu'>
+        <div className="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <ul>
           <li><a href="#">News</a></li>
           <li><a href="#">Team</a></li>
